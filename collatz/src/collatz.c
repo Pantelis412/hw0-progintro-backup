7188b5ebcfc8f73321847bc6ed257bc90cc4304f
Pantelis412 <147104690+Pantelis412@users.noreply.github.com>
2023-11-08 21:40:06 +0200

    added comments

--- collatz/src/collatz.c
+++ collatz/src/collatz.c
@@ -3,12 +3,12 @@
 
 int main(int argc, char **argv)
 {
-    if (argc != 3)
-        printf("Programm needs to be called as : ./Collatz left_limit right_limit");
+    if (argc != 3) /*check if the programm has the correct amount of arguments*/
+        printf("Programm needs to be called as : ./Collatz left_limit right_limit"); /*print error message*/
 
     int left_limit = atoi(argv[1]);
     int right_limit = atoi(argv[2]);
-    int *array = malloc(sizeof(int) * 100000000);
+    int *array = malloc(sizeof(int) * 100000000); /*array declaration in dynamic format*/
 
     if (left_limit < 1 || right_limit < 1)
     { /*If one of the limits is non-positive the programm prints "0" and terminates*/
@@ -17,16 +17,16 @@ int main(int argc, char **argv)
         return 1;
     }
     long long n;
-    int max_counter, temp_counter, i;
+    int max_counter, temp_counter, i;/*declaration of a temporary counter for the collatz sequence and of a max counter to save the max of all the temporary counters*/
     max_counter = 1;
     for (i = left_limit; i <= right_limit; i++)
-    {
+    {/*creation of a loop that checks every value in the range of the given limits*/
         temp_counter = 1;
-        n = i;
+        n = i;/*n is a temporary variable which takes the value of i and undergoes collatz sequence*/
 
         while (n != 1)
-        {
-            if (n<100000000 && array[n] != 0)
+        {/*creation of a loop to apply collatz sequence on the variable n*/
+            if (n<100000000 && array[n] != 0)/*check if n is below 100000000 (which is the given limit for the programm's arguments) and if so check if we have already saved the collatz number for the current value of n from previous loops*/
             {
                 temp_counter = array[n] + temp_counter - 1;
                 break;
@@ -37,16 +37,16 @@ int main(int argc, char **argv)
             else
             {
                 n = 3 * n + 1;
-                n = n / 2;
+                n = n / 2;/*if n is odd we can do two steps at once and add an extra unit to the temorary counter to skip some loops*/
                 temp_counter++;
             }
             temp_counter++;
         }
         
-        array[i] = temp_counter;
-        if (temp_counter > max_counter)
+        array[i] = temp_counter;/*we save the value of the temporary counter to this array so that we have it available for the next numbers that we are goint to check. Thus, we skip some loops. */
+        if (temp_counter > max_counter)/*we give the biggest value of all temporary counters to to the max counter*/
             max_counter = temp_counter;
     }
-    printf("%d\n", max_counter);
+    printf("%d\n", max_counter);/*we print the max counter*/
     return 0;
 }
